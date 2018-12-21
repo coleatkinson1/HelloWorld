0a5706d615ceb7ea71306ac99c392ee407a6c604
coleatkinson1 <40928853+coleatkinson1@users.noreply.github.com>
2018-12-22 04:09:23 +1300

    Did some playing around

---
+++ src/Hello.scala
@@ -0,0 +1,38 @@
+
+
+object Hello extends App {
+
+  def cake(c: Int): Int = {
+    def addc(x: Int, y: Int) = x+y
+    def mulc(x: Int, y: Int) = x*y
+    mulc(c,3) * addc(1,2);
+  }
+
+  def fibo(c: Int = 1, p: Int = 0, m: Int): Any = {
+    if (c >= m) return
+    println(c+p)
+    fibo(c+p, c, m)
+  }
+
+  val pi = 3.141592;
+  val ops = Array((a: Double, b: Double) => { a - b}, (a: Double, b: Double) => { a + b})
+  def EstPi(c : Double = 4.0, i : Int = 1): Int = {
+    if (Math.abs(c-pi)<0.00001 || i >= 99999999){ println(s"PI estimate: ${c}"); return i; }
+    var j = 3 + ((i-1)*2)
+    var r  = {
+      i % 2 match {
+        case 0 => c + (4.0/(j))
+        case 1 => c - (4.0/(j))
+      }
+    }
+    EstPi(r, i+1)
+  }
+
+  println(s"Cake result is: ${cake(3)}")
+
+  var f = 20
+  println(s"Fibonacci sequence until ${f}")
+  fibo(m = f)
+
+  println(s"Iterations to guess pi to ${s"${pi}".length()-1} places: ${EstPi()}")
+}
